e6ed311e80604edd2e74ab965e25f4e898496827
ninefloor <less0805@gmail.com>
2025-05-29 15:09:37 +0000

    chore: 주석 수정

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 /**
  * @usage 원하는 곳에서 useToast 훅 호출 및 사용
- *   const toastOpen = useToast({ title: '토스트 테스트', desc: 'desc', type: ToastType.Default });
+ *   const toastOpen = useToast();
  * => toastOpen({title:'토스트 제목', desc:'토스트 내용', type: ToastType.Default})
  */
 
